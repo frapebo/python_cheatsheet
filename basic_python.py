edd489af844c244a362436b41695c2af02bc4691
frapebo <frapebo@gmail.com>
2021-05-14 15:59:55 +0200

    Update basic_python.py

--- basic_python.py
+++ basic_python.py
@@ -1,1 +1,21 @@
 #basic python
+print("Hola mundo")
+
+variable = input("Como te llamas? ")
+
+print("Hola "+variable+"!")
+
+print("Ahora prueva esta calculadora")
+
+operacion = input("Que operacion quieres hacer? ")
+primernumero = input("primer numero: ")
+segundonumero = input("segundo numero: ")
+
+if operacion == "suma":
+    suma = int(primernumero)+int(segundonumero)
+    print(str(primernumero)+" + "+str(segundonumero)+" = "+str(suma))
+elif operacion == "multiplicacion":
+    multiplicacion = int(primernumero)*int(segundonumero)
+    print(str(primernumero)+" x "+str(segundonumero)+" = "+str(multiplicacion))
+else:
+    print("las operaciones disponibles son: suma, multiplicacion.")
